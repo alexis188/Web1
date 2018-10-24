6e5b8add467511f2666873013e1d7a7b6409ef5a
alexis188 <44430673+alexis188@users.noreply.github.com>
2018-10-24 11:14:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function myFunction() {
+    alert("Esto es javascript!");
+}
+
+
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function tetxfunction(){texfield.secureTextEntry = true;}
+
+function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+        
+function showfunction() {
+    document.getElementById("myUL").style.visibility = "visible";
+}
+    }
+}
